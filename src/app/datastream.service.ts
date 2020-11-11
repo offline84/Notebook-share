42b2286e0f5b1e3bf86c0ae2573be5bd46c6fe39
offline84 <ken_decrop@hotmail.com>
2020-11-11 02:46:57 +0100

    added add user function

--- src/app/datastream.service.ts
+++ src/app/datastream.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,7 +8,12 @@ export class DatastreamService {
 
   constructor(private http: HttpClient) { }
 
-  getUsersFromDb =() =>{
+  getUsersFromDb = () => {
     return this.http.get('https://clear-diagnostic-inspiration.glitch.me/users');
+  }
+
+  addUserToDb = (newUser) => {
+
+    return this.http.post('https://clear-diagnostic-inspiration.glitch.me/users', {'name': newUser});
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { DatastreamService } from '../datastream.service';
 
 @Component({
@@ -8,17 +10,39 @@ import { DatastreamService } from '../datastream.service';
 })
 export class UserComponent implements OnInit {
   @Input() name: string;
+  userdata: any;
+  userSubject: BehaviorSubject<any> =new BehaviorSubject<any>(null);
   users: any;
+  count: number = -1;
 
-  constructor(private data: DatastreamService) {
-    data.getUsersFromDb().subscribe((usersData) =>{
-      this.users = usersData;
-      console.log(this.users);
-    });
+
+  constructor(private dataStream: DatastreamService) {
+    this.getUsers();
+
+    this.users = this.userSubject.asObservable();
+    this.users.subscribe(this.userdata);
   }
 
   ngOnInit(): void {
-
+  }
+  addUser = (user) =>{
+    this.dataStream.addUserToDb(user).subscribe((data) =>{
+      alert(user + ' is toegevoegd!');
+    },
+    error =>{
+      console.log(error);
+    });
+    this.userSubject.next(data =>{
+      console.log(data);
+    });
+    setTimeout(this.getUsers,10000);
   }
 
+  getUsers = () =>{
+
+    this.dataStream.getUsersFromDb().subscribe((data) =>{
+      this.userdata = data;
+      console.log(this.userdata);
+    });
+  }
 }
